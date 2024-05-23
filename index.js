b8f7f904e1f19841e6430224c94dd663b9f6e816
Joey Litten <109524827+JoeyLitten@users.noreply.github.com>
2024-05-23 14:31:23 -0400

    Changed deck array and added new ui

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ let player = {
     name: "Per",
     chips: 200
 }
-
+let deck = ["A",1,2,3,4,5,6,7,8,9,10,"J","Q","K"]
 let cards = []
+let hand = []
 let sum = 0
 let hasBlackJack = false
 let isAlive = false
@@ -12,10 +13,12 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 let playerEl = document.getElementById("player-el")
+let playBtn = document.getElementById("play-btn")
 
 playerEl.textContent = player.name + ": $" + player.chips
+playBtn.textContent = "START GAME"
 
-function getRandomCard() {
+/* function getRandomCard() {
     let randomNumber = Math.floor( Math.random()*13 ) + 1
     if (randomNumber > 10) {
         return 10
@@ -24,19 +27,43 @@ function getRandomCard() {
     } else {
         return randomNumber
     }
+} */
+function getRandomCardFromDeck(){
+    let random =  Math.floor(Math.random() * deck.length)
+    
+ if(deck.random === "J" || deck.random === "Q" || deck.random === "K"){
+    return 10
+}else if ( deck.random  == "A"){
+    return 1
+}else{
+    return deck[random]
+}   
 }
+
+/* function turnFacesToNumbers(){
+    for( let i = 0; i < cards.length; i++){
+    if(i == "J" || i == "Q" || i == "K"){
+        return 10
+    }else if ( i == "A"){
+        return 11
+    }else{
+        return i
+    } 
+    }
+}  */
 
 function startGame() {
     isAlive = true
-    let firstCard = getRandomCard()
-    let secondCard = getRandomCard()
-    cards = [firstCard, secondCard]
+    let firstCard = getRandomCardFromDeck()
+    let secondCard = getRandomCardFromDeck()
+    cards = [firstCard, secondCard]   
     sum = firstCard + secondCard
     renderGame()
+    //dispellPlayButton()
 }
 
 function renderGame() {
-    cardsEl.textContent = "Cards: "
+    cardsEl.textContent = "Hand: "
     for (let i = 0; i < cards.length; i++) {
         cardsEl.textContent += cards[i] + " "
     }
@@ -47,9 +74,11 @@ function renderGame() {
     } else if (sum === 21) {
         message = "You've got Blackjack!"
         hasBlackJack = true
+        //castPlayButton()
     } else {
         message = "You're out of the game!"
         isAlive = false
+       // castPlayButton()
     }
     messageEl.textContent = message
 }
@@ -57,9 +86,17 @@ function renderGame() {
 
 function newCard() {
     if (isAlive === true && hasBlackJack === false) {
-        let card = getRandomCard()
-        sum += card
+        let card = getRandomCardFromDeck()
         cards.push(card)
-        renderGame()        
+        sum += card
+        renderGame() 
     }
 }
+
+/* function castPlayButton(){
+    document.getElementById("play-btn").style.display = ""
+}
+
+function dispellPlayButton(){
+    document.getElementById("play-btn").style.display = "none"
+} */
